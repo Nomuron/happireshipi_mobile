424446c03473d75cb7b4f9d885c14dbc2dff5b75
Alicja Szczypior <alicjaszczypior@MacBook-Pro-Alicja-2.local>
2024-05-22 00:29:17 +0200

    Recipes list layout and recyclerView introduction

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,14 +8,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.recipes.adapters.RecipesAdapter;
+import com.example.myapplication.recipes.control.RecipesProvider;
+import com.example.myapplication.recipes.entity.Recipe;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecipesAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Resources resources = getResources();
+        List<Recipe> recipes = RecipesProvider.getRecipes(resources);
+
+        recyclerView = findViewById(R.id.recyclerViewDishesList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        adapter = new RecipesAdapter(this, recipes);
+        recyclerView.setAdapter(adapter);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
---
+++ app/src/main/java/com/example/myapplication/recipes/adapters/RecipesAdapter.java
@@ -0,0 +1,76 @@
+package com.example.myapplication.recipes.adapters;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.graphics.drawable.Drawable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.myapplication.R;
+import com.example.myapplication.recipes.entity.Recipe;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+public class RecipesAdapter extends RecyclerView.Adapter<RecipesAdapter.MyViewHolder> {
+    private Context context;
+    private List<Recipe> recipes;
+
+//    AssetManager assets;
+
+
+    public RecipesAdapter(Context context, List<Recipe> recipes) {
+        this.context = context;
+        this.recipes = recipes;
+//        this.assets = context.getAssets();
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.dish_tile, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Recipe recipe = recipes.get(position);
+        holder.recipeName.setText(recipe.getName());
+        holder.portionsNumberInput.setHint("1");
+//        try (InputStream stream =
+//                     assets.open("images/" + recipe.getImageFileName())) {
+//            Drawable recipeImg = Drawable.createFromStream(stream, recipe.getImageFileName());
+//            holder.recipeImage.setImageDrawable(recipeImg);
+//
+//        } catch (IOException exception) {
+//            Log.e(recipe.getName(), "Error loading " + recipe.getImageFileName());
+//        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return recipes.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView recipeName;
+        EditText portionsNumberInput;
+//        ImageView recipeImage;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            recipeName = itemView.findViewById(R.id.recipeName);
+            portionsNumberInput = itemView.findViewById(R.id.portionsNumberInput);
+//            recipeImage = itemView.findViewById(R.id.recipeImage);
+        }
+    }
+}
